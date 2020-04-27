87bc9834bdea0331ba777e38e6f6a01200affc25
王强 <249749871@qq.com>
2020-04-27 17:51:13 +0800

    upd

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const Pen = require('./lib/pen');
 const Downloader = require('./lib/downloader');
 const util = require('./lib/util');
 
-export * from Pen, Downloader, util
+module.exports = {
+  Pen,
+  Downloader,
+  util
+}
